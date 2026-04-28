4bb3394127e0cda9ef478f163b6e014453026d2b
Teddy Sears <tjsearszu@gmail.com>
2026-04-28 13:13:26 +0200

    video-114

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.js",
-    reporters: ["json"],
-    outputFile: "test.json",
-    include: ["src/**/*.test.{js,jsx}"],
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    reporters: ['json'],
+    outputFile: 'test.json'
   },
 });
